45a2ef20fa33d4f676143a3dbda27fffc314f09a
ruslannastasiev <ruslan0607@gmail.com>
2013-02-22 16:32:06 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OptimusPrime::Application.config.secret_token = 'c914133a979247de53bfe3fb761dc85ed05ec071b0a2379c8560f85df50a73d68860f152e5e8767720edb21b977b3f4a743171cd68be21bd0af1b1a160b49fb4'
