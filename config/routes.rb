cd435eda2abcc1de82b81581f54c093242bd1880
«ruslannastasiev» <ruslan0607@gmail.com>
2013-03-05 13:03:44 +0200

    formatted code

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 OptimusPrime::Application.routes.draw do
 
+  root :to => "users#new"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
 
-get "log_out" => "sessions#destroy", :as => "log_out"
-get "log_in" => "sessions#new", :as => "log_in"
-get "sign_up" => "users#new", :as => "sign_up"
-root :to => "users#new"
-resources :users
-resources :sessions
+  resources :users
+  resources :sessions
 
 
   # The priority is based upon order of creation:
