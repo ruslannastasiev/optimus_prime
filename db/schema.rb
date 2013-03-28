42f9e2c7c6e4e8b4b01840ddcecc5550ebfc806e
letline <letline1@gmail.com>
2013-03-28 10:04:18 +0200

    changed db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130316203301) do
+ActiveRecord::Schema.define(:version => 20130317153959) do
 
   create_table "championships", :force => true do |t|
     t.string   "name"
