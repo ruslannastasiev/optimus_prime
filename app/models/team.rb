01e2588b5996b6e74c63d8552aeac7c222708e52
«ruslannastasiev» <ruslan0607@gmail.com>
2013-04-17 15:16:09 +0300

    add league table

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
   attr_accessible :name
+  attr_reader :won, :lost, :drawn
 
   belongs_to :championship
   has_many :team1_scores, class_name: 'Score', foreign_key: :team1_id
@@ -8,4 +9,13 @@ class Team < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 100 }
   validates :championship_id, presence: true
 
+
+  def goal_for
+    team1_scores.sum(&:team1_score) + team2_scores.sum(&:team2_score)     
+  end
+
+  def goal_against
+    team1_scores.sum(&:team2_score) + team2_scores.sum(&:team1_score)        
+  end
+  
 end
